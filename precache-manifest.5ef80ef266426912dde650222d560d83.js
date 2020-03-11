051d7a129a50e5b7354b22dedc5ff773e9e9eadb
Puneet Johar <puneet.johar@gmail.com>
2020-03-11 15:01:48 +0000

    Updates

---
+++ precache-manifest.5ef80ef266426912dde650222d560d83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2c8d371b154ab793ae83e612046138",
+    "url": "/react-tut/index.html"
+  },
+  {
+    "revision": "091b76608caad3fee475",
+    "url": "/react-tut/static/css/main.f42fd2da.chunk.css"
+  },
+  {
+    "revision": "640fa3f2e5b37ee859bf",
+    "url": "/react-tut/static/js/2.c2a893ac.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-tut/static/js/2.c2a893ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091b76608caad3fee475",
+    "url": "/react-tut/static/js/main.51765925.chunk.js"
+  },
+  {
+    "revision": "19c8f6a71b3b0086b60e",
+    "url": "/react-tut/static/js/runtime-main.7cca48c1.js"
+  }
+]);
